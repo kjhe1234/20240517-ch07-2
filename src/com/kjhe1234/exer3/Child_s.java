c4714e819345b09f366750fe7708c494e3c0031c
kjhe1234 <kjhe1234@naver.com>
2024-05-17 16:05:49 +0900

    연습 문제 및 해답 추가

---
+++ src/com/kjhe1234/exer3/Child_s.java
@@ -0,0 +1,19 @@
+package com.kjhe1234.exer3;
+
+public class Child_s extends Parent_s {
+	private int studentNo;
+
+	public Child_s(String name, int studentNo) {
+		super(name);
+		this.studentNo = studentNo;
+	}
+	
+//	public Child_s(String name, int studentNo) {
+//		super("홍길동");  // 부모클래스의 생성자를 반드시 호출 해줘야함
+//		this.name = name;
+//		this.studentNo = studentNo;
+//	}
+
+	
+	
+}
---
+++ src/com/kjhe1234/exer3/Parent_s.java
@@ -0,0 +1,19 @@
+package com.kjhe1234.exer3;
+
+public class Parent_s {
+	public String name;
+	
+	
+	
+//	public Parent_s() {
+//		super();
+//		// TODO Auto-generated constructor stub
+//	}
+
+
+	public Parent_s(String name) {
+		
+		this.name = name;
+	}
+
+}
